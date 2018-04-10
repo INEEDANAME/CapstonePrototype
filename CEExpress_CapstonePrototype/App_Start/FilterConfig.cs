c1b4185f5d712356d576402099ee677200c9a4f2
Dylan <mccowand@sheridancollege.ca>
2018-04-10 08:25:50 -0400

    Set-up the project

---
+++ CEExpress_CapstonePrototype/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CEExpress_CapstonePrototype
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
