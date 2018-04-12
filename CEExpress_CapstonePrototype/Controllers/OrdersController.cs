08665b5e546d870ca19885fcdcc5d04e438af7aa
Dylan <mccowand@sheridancollege.ca>
2018-04-12 17:15:53 -0400

    Added basic scaffolding for Viewing Orders, and specific order details.

---
+++ CEExpress_CapstonePrototype/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapstoneDBModel.Model;
+
+namespace CEExpress_CapstonePrototype.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private CEExpressDBEntities db = new CEExpressDBEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Client).Include(o => o.Shipment);
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
