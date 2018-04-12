ac283c7435941088e1d4378bf04711335467ed93
Dylan <mccowand@sheridancollege.ca>
2018-04-12 11:43:55 -0400

    Some minor changes

---
+++ CEExpress_CapstonePrototype/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CEExpress_CapstonePrototype.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
