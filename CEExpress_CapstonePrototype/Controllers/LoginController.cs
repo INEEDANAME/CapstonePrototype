e85724478f257f57fc5fbaada252baf5b47f886d
Dylan <mccowand@sheridancollege.ca>
2018-04-23 15:43:58 -0400

    Modified the logic for the login controller

--- CEExpress_CapstonePrototype/Controllers/LoginController.cs
+++ CEExpress_CapstonePrototype/Controllers/LoginController.cs
@@ -19,38 +19,17 @@ namespace CEExpress_CapstonePrototype.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Login(string uname, string pass)
-        //{
-        //    HttpClient client = new HttpClient();
-        //    client.BaseAddress = new Uri("https://api.dwaybill.com/2000105850/");
-        //    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-        //    HttpResponseMessage response = client.GetAsync("orders.json?v=1&key=f1d621905cece65bcbbb5018adacdd39adacdd39&customer_number=DYN833&password=pass").Result;
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        ViewBag.Message = "Success";
-        //        return RedirectToAction("Index", "Orders");
-        //    }
-        //    else
-        //        ViewBag.Message = "Failed";
-        //    return RedirectToAction("Index", "Home");
-        //}
-
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Login(userLogin login, string ReturnUrl="")
         {
 
             string message = "";
-
             using (CEExpressDBEntities de = new CEExpressDBEntities())
             {
                 var v = de.Clients.Where(a => a.accountNumber == login.AccountNumber).FirstOrDefault();
                 if (v != null)
                 {
-
-                    
                     if (string.Compare(login.Password, v.password) == 0)
                     {
                         int timeout = login.RememberMe ? 525600 : 20; //525600 min = 1 year
@@ -72,30 +51,29 @@ namespace CEExpress_CapstonePrototype.Controllers
                     }
                     else
                     {
-                        message = "Invalid credentials, please try again!";
+                        HttpClient client = new HttpClient();
+                        client.BaseAddress = new Uri("https://api.dwaybill.com/2000105850/");
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                        HttpResponseMessage response = client.GetAsync("orders.json?v=1&key=f1d621905cece65bcbbb5018adacdd39adacdd39&customer_number=DYN833&password=pass").Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            ViewBag.Message = "Success";
+                            return RedirectToAction("Index", "Orders");
+                        }
+                        else
+                        {
+                            message = "Invalid credentials, please try again!";
+                         
+                        }                   
                     }
                 }
                 else
                 {
-                    HttpClient client = new HttpClient();
-                    client.BaseAddress = new Uri("https://api.dwaybill.com/2000105850/");
-                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                    HttpResponseMessage response = client.GetAsync("orders.json?v=1&key=f1d621905cece65bcbbb5018adacdd39adacdd39&customer_number=DYN833&password=pass").Result;
-                    if (response.IsSuccessStatusCode)
-                    {
-                        ViewBag.Message = "Success";
-                        return RedirectToAction("Index", "Orders");
-                    }
-                    else
-                    {
-                        ViewBag.Message = "Failed";
-                        return RedirectToAction("Index", "Home");
-                    }
-                    //message = "Please enter valid credentials!";
-                }
-                ViewBag.Message = message;
-                return View();
+                    message = "Please enter valid credentials!";
+                }           
             }
+                ViewBag.Message = message;
+                return View();     
         }
 
         //Logout
