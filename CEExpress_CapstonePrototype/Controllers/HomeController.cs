f207016fa04716b0ea9ca255a8caf75a4eb9b681
Mahadevan Ramakrishnan <mahadevanr90@gmail.com>
2018-04-23 15:15:54 -0400

    MD - Login implementation - in Progress

--- CEExpress_CapstonePrototype/Controllers/HomeController.cs
+++ CEExpress_CapstonePrototype/Controllers/HomeController.cs
@@ -8,7 +8,8 @@ namespace CEExpress_CapstonePrototype.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [Authorize]
+            public ActionResult Index()
         {
             return View();
         }
