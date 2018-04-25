60725e246957618e0fe67375df14469454de8431
Dylan <mccowand@sheridancollege.ca>
2018-04-24 20:28:36 -0400

    Added images and view controllers to display the wireframe prototypes for management users

---
+++ CEExpress_CapstonePrototype/Controllers/ManagementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CEExpress_CapstonePrototype.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManagementReport()
+        {
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            return View()
+        }
+
+        public ActionResult ClientReports()
+        {
+            return View();
+        }
+    }
+}
